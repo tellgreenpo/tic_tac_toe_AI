8c956ae6b3772208e7061038d689b5afbfb12d26
tellgreenpo <60693210+tellgreenpo@users.noreply.github.com>
2020-04-30 19:50:18 +0200

    ia_choix fixed and some minor tweeks

--- IA_Morpion2.py
+++ IA_Morpion2.py
@@ -1,11 +1,6 @@
 import random
 
-#je laisse tomber les rotations pour pas a avoir
-#def rotation(array): #roatation simple du tableau de jeu
-    #line1 = [array[0][2], array[1][2], array[2][2]]
-    #line2 = [array[0][1], array[1][1], array[2][1]]
-    #line3 = [array[0][0], array[1][0], array[2][0]]
-    #return [line1, line2, line3]
+# je laisse tomber les rotations car il faut les faire ensuite pour le choix
 
 
 class IA:
@@ -15,59 +10,70 @@ class IA:
         self.tbl_positions = []
         self.tbl_memory = []
 
-    def index(self, tableau): # On suppose que le tableau est deja present
+    def indice(self, tableau): # On suppose que le tableau est deja present
         return self.tbl_positions.index(tableau) #retourne index du tableau entre en argument
 
     def modif_memoire(self, save, partie_perdue): #prend en argument la situation ulterieur a la defaite
         if partie_perdue:
-            index = self.index(self,save)
+            index = self.indice(save)
             ligne = self.previous_choice[0]
             colonne = self.previous_choice[1]
             self.tbl_memory[index][ligne][colonne] = 0
 
 
-    def add_inconnu(self, tableau):
-        if tableau not in self.tbl_positions: #regarder si la disposition existe ou pas dans la memoire
-            self.tbl_positions[:] = self.tbl_positions + [tableau] #modifie totalement la base memoire en incrementant(au cas ou mdrr)
-            #self.tbl_positions.append(self,tableau) ?
-
     def case_occupee(self,tableau):
-        i = self.index(self,tableau)
-        for line in tableau:
-            for case in line:
-                if case != 0 and self.tbl_memory[i][line][case] == 1:
+        i = self.indice(tableau)
+        for line in [0,1,2]:
+            for case in [0,1,2]:
+                # met proba a zero si la case est occupe et sa proba n'est pas deja a 0
+                if tableau[line][case] != 0 and self.tbl_memory[i][line][case] == 1:
                     self.tbl_memory[i][line][case] = 0
+
+
+    def add_inconnu(self, tableau):
+        if tableau not in self.tbl_positions: # regarder si la disposition existe ou pas dans la memoire
+            self.tbl_positions[:] = self.tbl_positions + [tableau] # modifie totalement la base memoire en incrementant
+            self.tbl_memory[:] = self.tbl_memory + [[[1,1,1],[1,1,1],[1,1,1]]] # ajoute proba memoire a la fin
+            self.case_occupee(tableau)
+
 
     def ia_choix(self, tableau): #retourne une liste avec la ligne et la case choisie
         number = 0
-        line = 0
-        case = 0
+        ligne = 0
         found = False
-        index = self.index(self,tableau)
+        arrayindextot = []
+        arrayindex = []
+        index = self.indice(tableau)
         number = sum(x.count(1) for x in self.tbl_memory[index]) #verifie le nombre de choix non perdants
         if number == 1: #si il y en a 1 seul on le cherche et on assigne sa ligne et sa case comme element d'une liste
-            while not found and line < 3:
-                while not found and case < 3:
-                    if self.tbl_memory[index][line][case] == 1:
-                        found = True
+            for line in self.tbl_memory[index]:
+                colonne = 0
+                for case in line:
+                    if case == 1:
+                        break
                     else:
-                        case += 1
-                line += 1
-            choice = [line,case]
+                        colonne += 1
+                else:
+                # s'execute si la boucle se termine normalement => https://psung.blogspot.com/2007/12/for-else-in-python.html
+                    ligne += 1
+                    continue
+                break
+            choice = [ligne, colonne]
         elif number == 0: #aucun choix non perdant
-            self.modif_memoire(self,self.previous_state,True) # si toutes les probas sont a 0, on met a zero le choix qui emmene a cette situation
+            self.modif_memoire(self.previous_state,True) # si toutes les probas sont a 0, on met a zero le choix qui emmene a cette situation
             choice = [0,0]
         else:
             # On traverse chaque ligne
-            arrayindextot = []
-            arrayindex = []
-            for line in tableau:
-                arrayindex[:] = [i for i,x in enumerate(self.tbl_memory[index][line]) if x == 1]    #on cree une liste avec les indexs des choix non perdants
-                     # on ajoute cette liste par ligne a une liste general qui contient a la fin les liste des index de choix non perdants par ligne
-                arrayindextot.append(arrayindex)
-            choice_line = random.choice(arrayindextot) # on choisit au hazard une ligne avec au moins un choix non perdant
-            choice_case = random.choice(choice_line) #on choisit au hazard une case de la ligne
-            choice = [choice_line,choice_case]
+            for line in [0,1,2]:
+                # on cree une liste avec les indices des choix non perdants
+                arrayindex = [i for i,x in enumerate(self.tbl_memory[index][line]) if x == 1]
+                # on ajoute cette liste par ligne a une liste general
+                # qui contient a la fin les liste des index de choix non perdants par ligne
+                if arrayindex: # c'est la meme chose que if arrayindex != [], les sequences vides sont consideres comme False
+                    for case in arrayindex:
+                        arrayindextot.append([line] + [case])
+            print(arrayindextot)
+            choice = random.choice(arrayindextot)
         #on met a jour l'etat ou l'on est pour le sauvegarder et le choix fait
         self.previous_state = tableau
         self.previous_choice = choice
